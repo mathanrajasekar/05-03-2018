ce7f1263176c909c61a3594982a6cee1dd599d9e
mathanrajasekar <35525911+mathanrajasekar@users.noreply.github.com>
2018-03-05 15:44:48 +0530

    Create numeric.c
    validate if a string is numeric.

---
+++ numeric.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char s;
+printf("Enter the value of s");
+scanf("%d",&s);
+if(s>=0&&s<=9)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+printf("yes" or "no");
+getch();
+}
